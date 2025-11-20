425882c1e610c31316c476438a706f3e7004f173
Nagy Szabolcs <dungscer@gmail.com>
2025-11-20 13:55:46 +0100

    Clear project for multi platform building

--- Engine/build.sh
+++
@@ -1,2 +0,0 @@
-g++ $(find src -name "*.cpp") -o engine -Iinclude $(sdl2-config --cflags --libs) -Wall -Wextra -g
-#g++ $(find src -name "*.cpp") -o engine.exe -Iinclude $(sdl2-config --cflags --libs) -Wall -Wextra -g
